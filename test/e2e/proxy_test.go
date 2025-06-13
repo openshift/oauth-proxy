61c7849f2499a1b9d7f96c19e1adc9c462f0ca9e
Krzysztof Ostrowski <kostrows@redhat.com>
2025-06-13 19:11:20 +0200

    e2e: mv setup out of for, indent, ...

--- test/e2e/proxy_test.go
+++ test/e2e/proxy_test.go
@@ -7,7 +7,6 @@ import (
 	"crypto/x509"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"net/http/cookiejar"
 	"os"
@@ -24,32 +23,60 @@ import (
 	"k8s.io/client-go/rest"
 
 	configclient "github.com/openshift/client-go/config/clientset/versioned"
-	projectclient "github.com/openshift/client-go/project/clientset/versioned"
 	routeclient "github.com/openshift/client-go/route/clientset/versioned"
 	userclient "github.com/openshift/client-go/user/clientset/versioned"
 )
 
 func TestOAuthProxyE2E(t *testing.T) {
-	ctx := context.Background()
-
 	testConfig := NewClientConfigForTest(t)
 	kubeClient, err := kubernetes.NewForConfig(testConfig)
 	require.NoError(t, err)
+
+	ns, cancel := CreateTestProjectWithCancel(t, kubeClient)
+	defer cancel()
+
+	registry := strings.Split(os.Getenv("RELEASE_IMAGE_LATEST"), "/")[0]
+	require.NotEmpty(t, registry, "Registry is empty. Check RELEASE_IMAGE_LATEST environment variable.")
+	namespace := os.Getenv("NAMESPACE")
+	require.NotEmpty(t, namespace, "Namespace is empty. Check NAMESPACE environment variable.")
+	image := registry + "/" + namespace + "/pipeline:oauth-proxy"
+	t.Logf("Using custom debug image: %s", image)
+
+	t.Log("Removing kubeadmin user if exists")
+	kubeadminSecret, err := kubeClient.CoreV1().Secrets("kube-system").
+		Get(context.TODO(), "kubeadmin", metav1.GetOptions{})
+	var kubeadminExisted bool
+	if err != nil && !errors.IsNotFound(err) {
+		t.Fatalf("couldn't check for kubeadmin user: %v", err)
+	}
+	if err == nil {
+		kubeadminExisted = true
+		t.Log("kubeadmin user found, backing up for restoration")
+	}
+
+	err = kubeClient.CoreV1().Secrets("kube-system").
+		Delete(context.TODO(), "kubeadmin", metav1.DeleteOptions{})
+	if err != nil && !errors.IsNotFound(err) {
+		t.Fatalf("couldn't remove the kubeadmin user: %v", err)
+	}
+
+	if kubeadminExisted {
+		defer func() {
+			t.Log("Restoring kubeadmin user")
+			kubeadminSecret.ResourceVersion = ""
+			kubeadminSecret.UID = ""
+			_, err := kubeClient.CoreV1().Secrets("kube-system").
+				Create(context.TODO(), kubeadminSecret, metav1.CreateOptions{})
+			if err != nil {
+				t.Errorf("Failed to restore kubeadmin user: %v", err)
+			}
+		}()
+	}
+
 	configClient, err := configclient.NewForConfig(testConfig)
-	require.NoError(t, err)
-	projectClient, err := projectclient.NewForConfig(testConfig)
-	require.NoError(t, err)
-	routeClient, err := routeclient.NewForConfig(testConfig)
 	require.NoError(t, err)
 	userClient, err := userclient.NewForConfig(testConfig)
 	require.NoError(t, err)
-	ns := CreateTestProject(t, kubeClient, projectClient)
-	defer func() {
-		if len(os.Getenv("DEBUG_TEST")) > 0 {
-			return
-		}
-		kubeClient.CoreV1().Namespaces().Delete(ctx, ns, metav1.DeleteOptions{})
-	}()
 
 	testCases := []struct {
 		name          string
@@ -172,91 +199,115 @@ func TestOAuthProxyE2E(t *testing.T) {
 		},
 	}
 
-	registry := strings.Split(os.Getenv("RELEASE_IMAGE_LATEST"), "/")[0]
-	require.NotEmpty(t, registry, "Registry is empty. Check RELEASE_IMAGE_LATEST environment variable.")
-	namespace := os.Getenv("NAMESPACE")
-	require.NotEmpty(t, namespace, "Namespace is empty. Check NAMESPACE environment variable.")
-	image := registry + "/" + namespace + "/pipeline:oauth-proxy"
-
-	// get rid of kubeadmin user to remove the additional step of choosing an idp
-	err = kubeClient.CoreV1().Secrets("kube-system").Delete(context.TODO(), "kubeadmin", metav1.DeleteOptions{})
-	if err != nil && !errors.IsNotFound(err) {
-		t.Fatalf("couldn't remove the kubeadmin user: %v", err)
-	}
-
-	users, idpCleanup := createTestIdP(t, kubeClient, configClient.ConfigV1().OAuths(), userClient, ns, len(testCases))
-	defer func() {
-		if len(os.Getenv("DEBUG_TEST")) == 0 {
-			idpCleanup()
-		}
-	}()
+	users, idpCleanup := createTestIdP(t, kubeClient,
+		configClient.ConfigV1().OAuths(), userClient, ns, len(testCases))
+	t.Logf("Created test IdP with %d users", len(users))
+	defer idpCleanup()
 
 	// wait for the IdP to be honored in the oauth-server
-	WaitForClusterOperatorStatus(t, configClient.ConfigV1(), nil, pbool(true), nil)
-	WaitForClusterOperatorStatus(t, configClient.ConfigV1(), pbool(true), pbool(false), nil)
+	t.Log("Waiting for IdP to be honored in oauth-server")
+	err = WaitForClusterOperatorStatus(t, configClient.ConfigV1(), nil,
+		pbool(true), nil)
+	require.NoError(t, err, "Error waiting for oauth-server operator to be ready")
+	err = WaitForClusterOperatorStatus(t, configClient.ConfigV1(),
+		pbool(true), pbool(false), nil)
+	require.NoError(t, err, "Error waiting for oauth-server operator to be ready")
 
-	t.Logf("test image: %s, test namespace: %s", image, ns)
+	routeClient, err := routeclient.NewForConfig(testConfig)
+	require.NoError(t, err)
+
+	openshiftTransport, err := rest.TransportFor(testConfig)
+	require.NoError(t, err)
 
 	backendImage := "nginxdemos/nginx-hello:plain-text"
-	currentTestIdx := 0 // to pick the current user so that each test gets a fresh grant
-	for _, tc := range testCases {
-		runOnly := os.Getenv("TEST")
-		if len(runOnly) > 0 && runOnly != tc.name {
-			continue
-		}
 
+	upstreamCA, upstreamCert, upstreamKey, err := createCAandCertSet("localhost")
+	require.NoError(t, err, "Error creating upstream TLS certificates")
+
+	for i, tc := range testCases {
 		t.Run(fmt.Sprintf("setting up e2e tests %s", tc.name), func(t *testing.T) {
-			_, err := kubeClient.CoreV1().ServiceAccounts(ns).Create(ctx, newOAuthProxySA(tc.name), metav1.CreateOptions{})
+			ctx := context.Background()
+			user := users[i]
+			t.Logf("Using test user: %s", user)
+
+			sa, err := kubeClient.CoreV1().ServiceAccounts(ns).Create(
+				ctx, newOAuthProxySA(tc.name), metav1.CreateOptions{},
+			)
 			if err != nil {
 				t.Fatalf("setup: error creating SA: %s", err)
 			}
+			defer func() {
+				_ = kubeClient.CoreV1().ServiceAccounts(ns).
+					Delete(ctx, sa.Name, metav1.DeleteOptions{})
+			}()
 
-			proxyRouteHost := createOAuthProxyRoute(t, routeClient.RouteV1().Routes(ns), tc.name)
+			proxyRouteHost := createOAuthProxyRoute(t,
+				routeClient.RouteV1().Routes(ns), tc.name)
+			defer func() {
+				_ = deleteTestRoute(t, routeClient.RouteV1().Routes(ns), fmt.Sprintf("proxy-route-%s", tc.name))
+			}()
 
-			// Create the TLS certificate set for the client and service (with the route hostname attributes)
 			caPem, serviceCert, serviceKey, err := createCAandCertSet(proxyRouteHost)
 			if err != nil {
 				t.Fatalf("setup: error creating TLS certs: %s", err)
 			}
 
-			// Create the TLS certificate set for the proxy backend (-upstream-ca) and the upstream site
-			upstreamCA, upstreamCert, upstreamKey, err := createCAandCertSet("localhost")
-			if err != nil {
-				t.Fatalf("setup: error creating upstream TLS certs: %s", err)
-			}
-
-			_, err = kubeClient.CoreV1().Services(ns).Create(ctx, newOAuthProxyService(tc.name), metav1.CreateOptions{})
+			svc, err := kubeClient.CoreV1().Services(ns).
+				Create(ctx, newOAuthProxyService(tc.name), metav1.CreateOptions{})
 			if err != nil {
 				t.Fatalf("setup: error creating service: %s", err)
 			}
+			defer func() {
+				_ = kubeClient.CoreV1().Services(ns).
+					Delete(ctx, svc.Name, metav1.DeleteOptions{})
+			}()
 
-			// configMap provides oauth-proxy with the certificates we created above
-			_, err = kubeClient.CoreV1().ConfigMaps(ns).Create(ctx, newOAuthProxyConfigMap(ns, tc.name, caPem, serviceCert, serviceKey, upstreamCA, upstreamCert, upstreamKey), metav1.CreateOptions{})
+			cm, err := kubeClient.CoreV1().ConfigMaps(ns).Create(
+				ctx,
+				newOAuthProxyConfigMap(
+					ns, tc.name,
+					caPem,
+					serviceCert, serviceKey,
+					upstreamCA, upstreamCert, upstreamKey,
+				), metav1.CreateOptions{},
+			)
 			if err != nil {
 				t.Fatalf("setup: error creating certificate configMap: %s", err)
 			}
+			defer func() {
+				_ = kubeClient.CoreV1().ConfigMaps(ns).
+					Delete(ctx, cm.Name, metav1.DeleteOptions{})
+			}()
 
-			oauthProxyPod, err := kubeClient.CoreV1().Pods(ns).Create(ctx, newOAuthProxyPod(image, backendImage, tc.name, tc.proxyArgs), metav1.CreateOptions{})
+			t.Logf("Creating OAuth proxy pod with args: %v", tc.proxyArgs)
+			oauthProxyPod, err := kubeClient.CoreV1().Pods(ns).Create(
+				ctx,
+				newOAuthProxyPod(image, backendImage, tc.name, tc.proxyArgs),
+				metav1.CreateOptions{},
+			)
 			if err != nil {
-				t.Fatalf("setup: error creating oauth-proxy pod with image '%s' and args '%v': %s", image, tc.proxyArgs, err)
+				t.Fatalf("setup: error creating oauth-proxy pod with image '%s' and args '%v': %s",
+					image, tc.proxyArgs, err)
 			}
+			defer func() {
+				_ = kubeClient.CoreV1().Pods(ns).
+					Delete(ctx, oauthProxyPod.Name, metav1.DeleteOptions{})
+				_ = waitForPodDeletion(kubeClient, oauthProxyPod.Name, ns)
+			}()
 
 			err = waitForPodRunningInNamespace(kubeClient, oauthProxyPod)
 			if err != nil {
 				t.Fatalf("setup: error waiting for pod to run: %s", err)
 			}
 
-			openshiftTransport, err := rest.TransportFor(testConfig)
-			require.NoError(t, err)
-
 			host := "https://" + proxyRouteHost + "/oauth/start"
 			// Wait for the route, we get an EOF if we move along too fast
+			t.Logf("Waiting for route %s to be ready", host)
 			err = waitUntilRouteIsReady(t, openshiftTransport, host)
 			if err != nil {
 				t.Fatalf("setup: error waiting for route availability: %s", err)
 			}
 
-			user := users[currentTestIdx]
 			// For SAR tests the random user needs the admin role for this namespace.
 			t.Logf("Setting admin role for user %s in namespace %s", user, ns)
 			roleBinding := newOAuthProxyRoleBinding(user, ns)
@@ -270,22 +321,15 @@ func TestOAuthProxyE2E(t *testing.T) {
 					Delete(ctx, "sar-"+user, metav1.DeleteOptions{})
 			}()
 
-			defer func() {
-				t.Logf("cleaning up test %s", tc.name)
-				kubeClient.CoreV1().Pods(ns).Delete(ctx, "proxy", metav1.DeleteOptions{})
-				kubeClient.CoreV1().Services(ns).Delete(ctx, "proxy", metav1.DeleteOptions{})
-				deleteTestRoute(t, routeClient, "proxy-route")
-				kubeClient.CoreV1().ConfigMaps(ns).Delete(ctx, "proxy-certs", metav1.DeleteOptions{})
-				kubeClient.CoreV1().ServiceAccounts(ns).Delete(ctx, "proxy", metav1.DeleteOptions{})
-				waitForPodDeletion(kubeClient, oauthProxyPod.Name, ns)
-				execCmd("oc", []string{"adm", "policy", "remove-role-from-user", "admin", user, "-n", ns}, "")
-			}()
-
-			waitForHealthzCheck(t, openshiftTransport, "https://"+proxyRouteHost)
+			_ = waitForHealthzCheck(t, openshiftTransport, "https://"+proxyRouteHost)
 
 			check3DESDisabled(t, "https://"+proxyRouteHost, caPem)
 
-			err = testOAuthProxyLogin(t, openshiftTransport, proxyRouteHost, tc.accessSubPath, user, "password", tc.pageResult, tc.expectedErr, tc.bypass)
+			t.Logf("Testing OAuth proxy login with user %s to path %s",
+				user, tc.accessSubPath)
+			err = testOAuthProxyLogin(t, openshiftTransport, proxyRouteHost,
+				tc.accessSubPath, user, "password", tc.pageResult,
+				tc.expectedErr, tc.bypass)
 
 			if err == nil && len(tc.expectedErr) > 0 {
 				t.Errorf("expected error '%s', but test passed", tc.expectedErr)
@@ -293,18 +337,65 @@ func TestOAuthProxyE2E(t *testing.T) {
 
 			if err != nil {
 				if len(tc.expectedErr) > 0 {
-					if tc.expectedErr != err.Error() {
-						t.Errorf("expected error '%s', got '%s'", tc.expectedErr, err)
+					if strings.Contains(err.Error(), tc.expectedErr) {
+						t.Logf("Got expected error containing '%s': %s",
+							tc.expectedErr, err.Error())
+					} else {
+						t.Errorf("expected error containing '%s', got '%s'", tc.expectedErr, err)
 					}
 				} else {
 					t.Errorf("test failed with '%s'", err)
 				}
 			}
 		})
-
-		// increase the current user
-		currentTestIdx++
 	}
+
+}
+
+func testOAuthProxyLogin(t *testing.T, transport http.RoundTripper,
+	host, subPath, user, password, expectedResult, expectedErr string,
+	expectBypass bool) error {
+	t.Logf("Testing OAuth proxy login for host: %s, path: %s, user: %s",
+		host, subPath, user)
+	client := newHTTPSClient(t, transport)
+
+	if !expectBypass {
+		t.Log("Confirming OAuth flow (auth required)")
+		if err := confirmOAuthFlow(client, "https://"+host+subPath, user,
+			password, expectedErr, expectBypass); err != nil {
+			t.Logf("OAuth flow failed: %v", err)
+			return err
+		}
+	} else {
+	}
+
+	authenticateResp, err := client.Get("https://" + host + subPath)
+	if err != nil {
+		t.Logf("Failed to retrieve the base page: %v", err)
+		return fmt.Errorf("failed to retrieve the base page")
+	}
+	defer authenticateResp.Body.Close()
+
+	authenticatedContent, err := io.ReadAll(authenticateResp.Body)
+	require.NoError(t, err)
+
+	if authenticateResp.StatusCode != 200 {
+		return fmt.Errorf("expected to be authenticated, got status %q, page:\n%s",
+			authenticateResp.Status, string(authenticatedContent))
+	}
+
+	if authenticateResp.Request.Host != host {
+		return fmt.Errorf("did not reach upstream site - host mismatch")
+	}
+
+	if !strings.Contains(string(authenticatedContent), expectedResult) {
+		// don't print the whole returned page, it makes the test result unreadable
+		t.Fatalf("expected authenticated page to contain %s, but it's missing",
+			expectedResult)
+	}
+
+	t.Log("Page content contains expected result")
+	return nil
 }
 
 func confirmOAuthFlow(client *http.Client, requestURL, user, password,
@@ -411,49 +502,18 @@ func confirmOAuthFlow(client *http.Client, requestURL, user, password,
 	return nil
 }
 
-func testOAuthProxyLogin(t *testing.T, transport http.RoundTripper, host, subPath, user, password, expectedResult, expectedErr string, expectBypass bool) error {
-	client := newHTTPSClient(t, transport)
-
-	if !expectBypass {
-		if err := confirmOAuthFlow(client, "https://"+host+subPath, user, password, expectedErr, expectBypass); err != nil {
-			return err
-		}
-	}
-
-	authenticateResp, err := client.Get("https://" + host + subPath)
-	if err != nil {
-		return fmt.Errorf("failed to retrieve the base page")
-	}
-	defer authenticateResp.Body.Close()
-
-	// we should be authenticated now
-	if authenticateResp.StatusCode != 200 {
-		r, _ := ioutil.ReadAll(authenticateResp.Body)
-		return fmt.Errorf("expected to be authenticated, got status %q, page:\n%s", authenticateResp.Status, r)
-	}
-
-	if authenticateResp.Request.Host != host {
-		return fmt.Errorf("did not reach upstream site")
-	}
-
-	authenticatedContent, err := ioutil.ReadAll(authenticateResp.Body)
-	require.NoError(t, err)
-
-	if !strings.Contains(string(authenticatedContent), expectedResult) {
-		// don't print the whole returned page, it makes the test result unreadable
-		t.Fatalf("expected authenticated page to contain %s, but it's missing", expectedResult)
-	}
-
-	return nil
-}
-
 func check3DESDisabled(t *testing.T, proxyURL string, proxyCA []byte) {
+	t.Logf("Checking 3DES is disabled on: %s", proxyURL)
 	pool := x509.NewCertPool()
 	if !pool.AppendCertsFromPEM(proxyCA) {
 		t.Fatalf("error loading CA for client config")
 	}
 
-	jar, _ := cookiejar.New(nil)
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatalf("error creating cookie jar: %v", err)
+	}
+
 	tr := &http.Transport{
 		MaxIdleConns:    10,
 		IdleConnTimeout: 30 * time.Second,
@@ -463,7 +523,8 @@ func check3DESDisabled(t *testing.T, proxyURL string, proxyCA []byte) {
 				tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
 				tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
 			},
-			// TLS 1.3 uses specific cipher suites and ignores the cipher suite config above
+			// TLS 1.3 uses specific cipher suites and ignores the
+			// cipher suite config above
 			MaxVersion: tls.VersionTLS12,
 		},
 	}
@@ -476,4 +537,5 @@ func check3DESDisabled(t *testing.T, proxyURL string, proxyCA []byte) {
 	if !strings.Contains(err.Error(), "handshake failure") {
 		t.Fatalf("expected TLS handshake error with weak ciphers, got: %v", err)
 	}
+	t.Log("3DES check passed - connection properly rejected")
 }
